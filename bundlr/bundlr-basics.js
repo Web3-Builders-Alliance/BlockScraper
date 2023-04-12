c6b3c940809fda6d41b1894d4a6183e8502f7de7
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-12 22:44:09 +0000

    13 Nis 2023 01:44

--- bundlr/bundlr-basics.js
+++ bundlr/bundlr-basics.js
@@ -19,13 +19,15 @@ async function main() {
 	//const fundTx = await bundlr.fund(1e9);
 	//console.log('Fund successful, amount funded = ${fundTx.quantity}');
 
-	const balance = await bundlr.getLoadedBalance();
-	console.log('account funded balance=${balance.toString()}');
+	//const balance = await bundlr.getLoadedBalance();
+	//console.log('account funded balance=${balance.toString()}');
 
-	const decimalBalance = bundlr.utils.unitConverter(balance);
-	console.log('account funded decimal balance=${decimalBalance.toString()}');
+	//const decimalBalance = bundlr.utils.unitConverter(balance);
+	//console.log('account funded decimal balance=${decimalBalance.toString()}');
 
-	const pathToFile = "bundlr/article.png"
+	const pathToFile = "bundlr/article.png";
+	const uploadTx = await bundlr.uploadFile(pathToFile);
+	console.log('File uploaded URL=https://arweave.net/${uploadTx.id}');
 }
 
 main();
