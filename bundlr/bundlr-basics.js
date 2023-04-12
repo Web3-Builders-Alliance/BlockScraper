ec0844f50cdd67ab077d99053f3dec878c1a8b50
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-12 22:28:50 +0000

    13 Nis 2023 01:28

--- bundlr/bundlr-basics.js
+++ bundlr/bundlr-basics.js
@@ -16,7 +16,7 @@ async function main() {
 
 	console.log('Connected to node, wallet address ${bundlr.address}');
 
-	const fundTx = await bundlr.fund(1e18);
+	const fundTx = await bundlr.fund(1e9);
 }
 
 
