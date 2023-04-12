091e36b8f826c52db05ccd9ddf183955a3b7405a
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-12 22:03:43 +0000

    13 Nis 2023 01:03

--- bundlr/bundlr-basics.js
+++ bundlr/bundlr-basics.js
@@ -5,7 +5,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-	
+	const bundlr = new Bundlr.default(
+		"https://devnet.bundlr.network",
+		"solana",
+		process.env.PRIVATE_KEY,
+	);
 }
 
 
