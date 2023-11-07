989d4845806a7425bbb75f1f658cbaffdbe45074
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-08 01:04:43 +0300

    added images and created a basic structure

--- product/scraping-engine/components/Navbar.tsx
+++ product/scraping-engine/components/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full">
+      <nav className="nav">
+        <div className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo-icon.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
