b21f54ab24d27c23e9a38bbe6fd859d1a5cc1dfc
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-09 01:06:22 +0300

    project setup completed, building home page

---
+++ product/scraping-engine/components/HeroCarousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+
+
+const HeroCarousel = () => {
+  return (
+    <div>HeroCarousel</div>
+  )
+}
+
+export default HeroCarousel
