d39cbf0b6d5991d91fa7304c905ffb0ffb6de15a
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-12 22:15:43 +0300

    started after reorganization

--- product/website/src/app/page.tsx
+++ product/website/src/app/page.tsx
@@ -1,5 +1,7 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <div></div>
+    <MaxWidthWrapper className = "bg-red-500"><div></div></MaxWidthWrapper>
   )
 }
