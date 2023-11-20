afeac81162fbb44292978b24fb638e32b3c6ab2b
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-20 12:05:55 +0300

    tRPC setup completed

--- product/website/src/app/auth-callback/page.tsx
+++ product/website/src/app/auth-callback/page.tsx
@@ -1,4 +1,5 @@
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
 
 const Page = async () => {
     const router = useRouter()
@@ -6,8 +7,14 @@ const Page = async () => {
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
 
-    const apiResponse = await fetch('/api/whatever')
-    const data : {name: string} = await apiResponse.json()
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({success}) => {
+            if(success) {
+                //user is synced to db
+                router.push(origin ? '/${origin}': '/dashboard')
+            }
+        }
+    })
 }
 
 export default Page
