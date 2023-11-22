9c7cd84cea4ed3a0bc971d24a9621fc85dd83a78
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-22 16:33:19 +0300

    troubleshooting

--- product/website/src/app/dashboard/page.tsx
+++ product/website/src/app/dashboard/page.tsx
@@ -1,28 +1,15 @@
-import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/dist/server/api-utils";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation"
 
-const Page = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser(); // Make sure getUser is awaited to get the actual user object
+const Page = () => {
+  const {getUser} = getKindeServerSession()
+  const user = getUser()
 
-  if (!user || !user.id) {
-    redirect("/auth-callback?origin=dashboard");
-  }
+  if(!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
-  const dbUser = await db.user.findFirst({
-    where: {
-      id: user.id,
-    },
-  });
+  
 
-  if (!dbUser) {
-    // Redirect to auth-callback with status code 307 (Temporary Redirect)
-    redirect(307, "/auth-callback?origin=dashboard");
-  }
+  return <div>{user.email}</div>
+}
 
-  return <div>{user.email}</div>;
-};
-
-export default Page;
-
+export default Page
