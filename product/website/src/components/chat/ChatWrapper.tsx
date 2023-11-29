10d8d226dd18e0eadc408aeb59dd4d672ec53a16
Ata Tekeli <tekeliata2000@gmail.com>
2023-11-29 22:25:15 +0300

    building skeleton: chat wrapper

--- product/website/src/components/chat/ChatWrapper.tsx
+++ product/website/src/components/chat/ChatWrapper.tsx
@@ -1,13 +1,15 @@
+import ChatInput from "./ChatInput"
 import Messages from "./Messages"
 
 const ChatWrapper = () => {
-    return <div className = "relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+    return (
+    <div className = "relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className = "flex-1 justify-between flex flex-col mb-28">
             <Messages />
         </div>
 
-        <ChatInput />
-    </div>
+        <ChatInput isDisabled/>
+    </div>)
 }
 
 export default ChatWrapper
