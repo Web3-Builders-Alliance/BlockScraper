7f6de05e2e93737b0fbee177bace70dd5607c131
Ata Tekeli <tekeliata2000@gmail.com>
2023-12-07 00:59:04 +0300

    creating payments options with stripe

--- product/website/src/app/pricing/page.tsx
+++ product/website/src/app/pricing/page.tsx
@@ -1,12 +1,16 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import UpgradeButton from "@/components/UpgradeButton";
+import { buttonVariants } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { PLANS } from "@/config/stripe";
 import { cn } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight, Check, HelpCircle, Minus } from "lucide-react";
+import Link from "next/link";
 
 const Page = () => {
     const {getUser} = getKindeServerSession()
-    const user = getUser()
+    let user = getUser()
 
     const pricingItems = [
         {
@@ -91,7 +95,7 @@ const Page = () => {
             <div className = "pt-12 grid grid-cols-1 gap-10 lg:grid-cols-2">
                 <TooltipProvider>
                     {pricingItems.map(
-                        ({plan, tagline, quota, features}) => {
+                        async ({plan, tagline, quota, features}) => {
                         const price = PLANS.find(
                             (p) => p.slug === plan.toLowerCase()
                         )?.price.amount || 0
@@ -118,6 +122,92 @@ const Page = () => {
                                 <p className = "my-5 font-display text-6xl font-semibold">
                                     ${price}
                                 </p>
+                                <p className = "text-gray-500">per month</p>
+                            </div>
+
+                            <div className = "flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50">
+                              <div className = "flex items-center space-x-1">
+                                <p>{quota.toLocaleString()} all details included</p>
+
+                                <Tooltip delayDuration = {300}>
+                                  <TooltipTrigger className = "cursor-default ml-1.5">
+                                    <HelpCircle className = "h-4 w-4 text-zinc-500" />
+                                  </TooltipTrigger>
+                                  <TooltipContent className = "w-80 p-2">
+                                    Features for current membership tier.
+                                  </TooltipContent>
+                                </Tooltip>
+                              </div>
+                            </div>
+
+                            <ul className = "my-10 space-y-5 px-8">
+                              {features.map(({text, footnote, negative}) => (
+                                <li key = {text} className = "flex space-x-5">
+                                  <div className = "flex-shrink-0">
+                                    {negative ? (
+                                      <Minus className = "h-6 w-6 text-gray-300"/>
+                                    ) : (
+                                      <Check className = "h-6 w-6 text-blue-500"/>
+                                    )}
+                                  </div>
+                                  {footnote ? (
+                                    <div className = "flex items-center space-x-1">
+                                      <p 
+                                      className = {cn(
+                                        "text-gray-400", 
+                                        {
+                                        "text-gray-600": 
+                                        negative
+                                      })}>
+                                        {text}
+                                      </p>
+                                      <Tooltip delayDuration = {300}>
+                                  <TooltipTrigger className = "cursor-default ml-1.5">
+                                    <HelpCircle className = "h-4 w-4 text-zinc-500" />
+                                  </TooltipTrigger>
+                                  <TooltipContent className = "w-80 p-2">
+                                    {footnote}
+                                  </TooltipContent>
+                                </Tooltip>
+                                    </div>
+                                  ): 
+                                  <p 
+                                      className = {cn(
+                                        "text-gray-400", 
+                                        {
+                                        "text-gray-600": 
+                                        negative
+                                      })}>
+                                        {text}
+                                      </p>
+                                    }
+                                </li>
+                              ))}
+                            </ul>
+                            <div className = "border-t border-gray-200"/>
+                            <div className = "p-5">
+                              {plan === "Free" ? (
+                                <Link 
+                                href = {await user ? "/dashboard" : "/sign-in"}
+                                className = {buttonVariants({
+                                  className: "w-full",
+                                  variant: "secondary",
+                                })}>
+                                  {await user ? "Upgrade now" : "Sign up"}
+                                  <ArrowRight className = "h-5 w-5 ml-1.5"/>
+                                </Link>
+                              ) : await user ? (
+                                <UpgradeButton/>
+                              ) : (
+                                <Link 
+                                href = "sign-in" 
+                                className = {buttonVariants({
+                                  className: "w-full"
+                                })}>
+                                  {await user ? "Upgrade now" : "Sign up"}
+                                  <ArrowRight className = "h-5 w-5 ml-1.5"/>
+                                </Link>
+                              )}
                             </div>
                         </div>
                     }
