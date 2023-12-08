97c75069ba1e80b3d7d94a9901726bc855063504
Ata Tekeli <tekeliata2000@gmail.com>
2023-12-09 01:05:05 +0300

    subscription management logic complete

--- product/website/src/components/BillingForm.tsx
+++ product/website/src/components/BillingForm.tsx
@@ -1,16 +1,30 @@
-"use client"
+'use client'
 
-import { getUserSubscriptionPlan } from "@/lib/stripe"
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { useToast } from './ui/use-toast'
+import { trpc } from '@/app/_trpc/client'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+import { format } from 'date-fns'
 
 interface BillingFormProps {
-    subscriptionPlan: Awaited<
-        ReturnType<typeof getUserSubscriptionPlan>
-    >
+  subscriptionPlan: Awaited<
+    ReturnType<typeof getUserSubscriptionPlan>
+  >
 }
 
 const BillingForm = ({
-    subscriptionPlan,}: BillingFormProps) => {
-
+  subscriptionPlan,
+}: BillingFormProps) => {
+  const { toast } = useToast() 
 }
 
 export default BillingForm
